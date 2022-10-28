77e700d446e66e2e4d6d69a214ffecf3cdb65107
Viviana218 <104119912+Viviana218@users.noreply.github.com>
2022-10-28 07:29:55 -0500

    Inicializando

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import TodosProductos from './componentes/TodosProductos';
+import Producto from './componentes/Producto';
+import NuevoProducto from './componentes/NuevoProducto';
+import ActualizarProducto from './componentes/ActualizarProducto';
+
+
+const App = () => {
+  return (
+    <div className="container">
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact render={() => <TodosProductos/> } />
+          <Route path="/producto/:id" exact render={() => <Producto/> }/>
+          <Route path="/nuevo" render={() => <NuevoProducto/> }/>
+          <Route path="/producto/editar/:id" render={() => <ActualizarProducto/> }/>
+
+
+        </Switch>
+      </BrowserRouter>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/componentes/Producto.js
@@ -0,0 +1,43 @@
+import React, {useEffect, useState} from "react";
+import {Link, useHistory, useParams} from "react-router-dom";
+import axios from "axios";
+
+const Producto = () => {
+    const {id} = useParams();
+    const [producto, setProducto] = useState({});
+
+    const history = useHistory();
+
+    useEffect(() => {
+        axios.get("http://localhost:8000/api/productos/"+id)
+            .then(res => {
+                setProducto(res.data);
+            })
+            .catch(err => console.log(err));
+    }, [id]);
+
+    const borrarProducto = id => {
+        axios.delete("http://localhost:8000/api/productos/"+id)
+            .then(res => 
+                history.push("/")
+            )
+            .catch(err => console.log(err));
+    }
+
+    return(
+        <div className="card">
+            <div className="card-header">{producto.nombre}</div>
+            <div className="card-body">
+                <h2>${producto.precio}</h2>
+                <p>
+                    {producto.descripcion}
+                </p>
+                <Link to="/" className="btn btn-primary">Regresar</Link>
+                <Link to="/" className="btn btn-danger" onClick={() => borrarProducto(id)}>Eliminar</Link>
+            </div>
+        </div>
+    )
+
+}
+
+export default Producto;
